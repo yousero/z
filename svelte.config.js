5e3b7f626f6725407619a2d9a484d1ff08169b1b
yousero <youser8@gmail.com>
2025-05-17 19:15:32 +0500

    edit config

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
   kit: {
     adapter: adapter({
       pages: 'build',
@@ -12,7 +10,7 @@ const config = {
       precompress: false
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? 'z' : ''
+      base: process.env.NODE_ENV === 'production' ? '/<your-repo-name>' : ''
     },
     trailingSlash: 'always'
   }
