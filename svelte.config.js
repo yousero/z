2843719600a5152a10e6b390f30d204c8e2a952c
yousero <youser8@gmail.com>
2025-05-17 19:35:49 +0500

    new change

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false
-    }),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/z' : ''
-    },
-    trailingSlash: 'always'
-  }
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
 
 export default config;
